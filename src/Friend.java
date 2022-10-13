ad2f1a69fb7bf5591aa2c4698b87ef0c795bd705
weix0015 <weix0015@stud.kea.dk>
2022-10-13 15:09:57 +0200

    FriendsWithData

---
+++ src/Friend.java
@@ -0,0 +1,45 @@
+public class Friend {
+    private String name;
+    private String phoneNumber;
+    private String address;
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    // getter
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // setter
+    public void setPhoneNumber(String newphoneNumber) {
+        this.phoneNumber = newphoneNumber;
+    }
+
+    // getter
+    public String getAddress() {
+        return address;
+
+
+
+
+    }
+
+    // setter
+    public void setAddress(String newAdress) {
+        this.address = newAdress;
+    }
+    public Friend(String name, String phoneNumber, String address) {
+
+    }
+}
+
+
+
---
+++ src/TestFriend.java
@@ -0,0 +1,2 @@
+public class TestFriend {
+}
